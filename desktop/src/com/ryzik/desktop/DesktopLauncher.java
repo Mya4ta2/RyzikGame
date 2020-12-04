6631e5cc2428c021383713628a2c99d150112034
Mya4ta <sefce3543@gmail.com>
2020-12-04 23:49:01 +0300

    add icon

--- desktop/src/com/ryzik/desktop/DesktopLauncher.java
+++ desktop/src/com/ryzik/desktop/DesktopLauncher.java
@@ -1,12 +1,18 @@
 package com.ryzik.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ryzik.MainActivity;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainActivity(), config);
+		final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config) {
+			{
+				config.title = "Ryzik";
+				config.addIcon("icon.png", Files.FileType.Internal);
+			}
+		};
 	}
 }
